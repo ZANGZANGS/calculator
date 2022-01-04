f03b0537addb97d3fb21b01a05ea5fde36bbbf83
kimjangh94@gmail.com <kimjangh94@gmail.com>
2022-01-04 11:48:57 +0900

    컨트롤러 파라미터 어노테이션 변경 @ModelAttribute -> @RequestBody

--- src/main/java/wirebarley/calculator/exchange/controller/CalculatorController.java
+++ src/main/java/wirebarley/calculator/exchange/controller/CalculatorController.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import wirebarley.calculator.exchange.dto.CalculatorDto;
 import wirebarley.calculator.exchange.service.CalculatorService;
@@ -39,7 +39,7 @@ public class CalculatorController {
 
     @PostMapping("/convert")
     @ResponseBody
-    public Object convertController(@ModelAttribute CalculatorDto.Req req, Model model){
+    public Object convertController(@RequestBody CalculatorDto.Req req, Model model){
 
         return calculatorService.convertService(req);
     }
