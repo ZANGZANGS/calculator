91209a41a958f7784df9d3a30e09cd2d068810bf
kimjangh94@gmail.com <kimjangh94@gmail.com>
2022-01-09 15:39:47 +0900

    convert 서비스 테스트 코드 작성

---
+++ src/test/java/wirebarley/calculator/service/convert/ConvertServiceImplTest.java
@@ -0,0 +1,40 @@
+package wirebarley.calculator.service.convert;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConvertServiceImplTest {
+
+    @Autowired private ConvertService convertService;
+
+
+    @Test
+    void convert(){
+
+        Convert.Req req = Convert.Req.builder()
+                        .from("USD")
+                        .to("KRW")
+                        .amount(new BigDecimal(153))
+                        .rate(new BigDecimal(1101.28))
+                        .build();
+
+        Convert.Res res = convertService.convert(req);
+
+        System.out.println(res.getAmount().toString());
+        System.out.println(res.getAmount().toPlainString());
+        System.out.println(res.getAmount().toBigInteger());
+        System.out.println(res.getAmount().toEngineeringString());
+        System.out.println(res.getAmount().doubleValue());
+
+        Assertions.assertThat(res.getAmount().doubleValue()).isEqualTo(168495.84);
+
+
+    }
+}
